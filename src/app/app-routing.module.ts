b05d8091c16a189e256344133e0788e343bc3275
Bernhard Maier <bernhard.maier@werbas-ag.com>
2020-09-02 14:22:59 +0200

    Extended routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/taskboard'
+  },
+  {
+    path: 'taskboard',
+    loadChildren: () => import('./taskboard/taskboard.module').then(m => m.TaskboardModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
