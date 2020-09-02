57f45fb11d7feae984016427019d0eb8e0d05a98
Bernhard Maier <bernhard.maier@werbas-ag.com>
2020-09-02 14:36:19 +0200

    Added routing in taskboard

--- src/app/taskboard/taskboard.module.ts
+++ src/app/taskboard/taskboard.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskCardComponent } from './task-card/task-card.component';
 import { TaskCreateComponent } from './task-create/task-create.component';
+import { TaskboardComponent } from './taskboard.component';
+import { TaskboardRoutingModule } from './taskboard-routing.module';
 
 
 
 @NgModule({
-  declarations: [TaskListComponent, TaskCardComponent, TaskCreateComponent],
+  declarations: [TaskListComponent, TaskCardComponent, TaskCreateComponent, TaskboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TaskboardRoutingModule
   ]
 })
 export class TaskboardModule { }
